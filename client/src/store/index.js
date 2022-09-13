5163c7fea1da01b31b2f935f149db8c8e9cecb88
AndrewNagikh <nagikhandrew@gmail.com>
2022-09-13 14:18:09 +0300

    project-init

---
+++ client/src/store/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/prefer-default-export */
+import { legacy_createStore as createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import { reducers } from './reducers';
+
+const composeEnchancers = composeWithDevTools();
+
+export const store = createStore(reducers, composeEnchancers);
---
+++ client/src/store/reducers.js
@@ -0,0 +1,17 @@
+/* eslint-disable consistent-return */
+/* eslint-disable default-param-last */
+/* eslint-disable import/prefer-default-export */
+const initState = {
+
+};
+
+export const reducers = (state = initState, action) => {
+  switch (action.type) {
+    case '':
+
+      return state;
+
+    default:
+      break;
+  }
+};
