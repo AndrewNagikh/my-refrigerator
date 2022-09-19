cbc601769d1ad3fdf5d723ef8e0b53477c3abcbe
AndrewNagikh <nagikhandrew@gmail.com>
2022-09-19 09:58:24 +0300

    fridge recipe card nav

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import Header from './pages/Navigate/Header';
 import './App.css';
+import Cousines from './pages/Cousines/Cousines';
+import Types from './pages/Types/Types';
+import Refrigirator from './pages/Refrigirator/Refrigirator';
+import RecipePage from './pages/RecipePage/RecipePage';
 
 function App() {
   return (
-    <div className="App">
-      sssss
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <div className="container-xl">
+          <Routes>
+            <Route path="/" />
+            {/* <Route path="/auth" element={<Auth />} /> */}
+            <Route path="/recipe/:id" element={<RecipePage />} />
+            <Route path="/my-ref" element={<Refrigirator />} />
+            <Route path="/cuisine/:cuisine" element={<Cousines />} />
+            <Route path="/type/:type" element={<Types />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
